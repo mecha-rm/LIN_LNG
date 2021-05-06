5e99e4e3023ffdff2e00cccb188efe1ea8b16575
Roderick Montague <roderick.montague243@gmail.com>
2021-05-06 13:50:10 -0400

    C# NET Core Project
    Added a C# Net Core Project.

---
+++ LIN_LNG-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LIN_LNG_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
